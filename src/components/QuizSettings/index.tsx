7acfae8ad6f87a20142213b57e9fa8af0aed4409
Brooke <belv2c@gmail.com>
2021-03-23 14:42:17 -0500

    Added material ui styles to main settings page and edited tsconfig to use material ui

--- src/components/QuizSettings/index.tsx
+++ src/components/QuizSettings/index.tsx
@@ -1,22 +1,49 @@
-import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
+import React, { useEffect, useState } from "react"
 import { CategoriesType, Difficulty, SettingType, SettingPropsType } from "../../types"
 import { fetchQuizCategories } from "../../utils/quizUtil"
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  InputLabel,
+  FormControl,
+  Select,
+  TextField,
+  Button,
+  Box,
+} from "@material-ui/core";
 
+const useStyles = makeStyles({
+  root: {
+    justifyItems: "center",
+    alignItems: "center",
+    height: "100vh",
+    width: "100%",
+    display: "flex",
+  },
+  title: {
+    fontSize: 14,
+  },
+  textFields: {
+    width: "100%",
+    marginTop: "20px",
+  }
+});
+
+// TODO: refactor to not use React.FC
+// TODO: add error handling
 const QuizSettingsForm: React.FC<SettingPropsType> = ({
   newUserSetting,
   setSendRequest
 }) => {
   const [categories, setCategories] = useState<CategoriesType[]>([])
-  const { register, errors } = useForm<SettingType>()
+  const [newSetting, setUserSetting] = useState<SettingType>({
+      numberOfQuestions: 5,
+      difficulty: "easy",
+      category: 9,
+      categoryName: "General Knowledge",
+      name: "",
+    });
 
-    const [newSetting, setUserSetting] = useState<SettingType>({
-        numberOfQuestions: 5,
-        difficulty: "easy",
-        category: 9,
-        categoryName: "General Knowledge",
-        name: "",
-      });
+    const classes = useStyles();
 
     useEffect(() => {
       const getCategoriesData = async () => {
@@ -54,74 +81,90 @@ const QuizSettingsForm: React.FC<SettingPropsType> = ({
     }
 
     return (
-      <form onSubmit={handleFormSubmit}>
-        <div className="field">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            ref={register}
-            value={newSetting.name}
-            onChange={(e) => {
+      <div>
+        <div className={classes.root}>
+        <form onSubmit={handleFormSubmit}>
+          <div className="field">
+            <TextField
+              className={classes.textFields}
+              type="text"
+              id="standard-basic"
+              label="What's your name?"
+              value={newSetting.name}
+              variant="outlined"
+              onChange={(e) => {
+                  setUserSetting({
+                    ...newSetting,
+                    name: String(e.target.value),
+                  })
+              }}
+            />
+          </div>
+          <div className="field">
+            <TextField
+              className={classes.textFields}
+              type="number"
+              id="standard-basic"
+              label="Number of Questions"
+              name="numberOfQuestions"
+              value={newSetting.numberOfQuestions}
+              variant="outlined"
+              onChange={(e) => {
+                  setUserSetting({
+                    ...newSetting,
+                    numberOfQuestions: Number(e.target.value),
+                  })
+              }}
+            />
+          </div>
+          <FormControl className={classes.textFields}>
+            <InputLabel htmlFor="grouped-native-select">
+              Select Difficulty
+            </InputLabel>
+            <Select
+              id="grouped-native-select"
+              native
+              onChange={(e) => {
                 setUserSetting({
                   ...newSetting,
-                  name: String(e.target.value),
-                })
-            }}
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="numberOfQuestions">Number Of Questions</label>
-          <input
-            type="number"
-            id="numberOfQuestions"
-            name="numberOfQuestions"
-            ref={register({required: true})}
-            value={newSetting.numberOfQuestions}
-            onChange={(e) => {
-                setUserSetting({
-                  ...newSetting,
-                  numberOfQuestions: Number(e.target.value),
-                })
-            }}
-          />
-          {errors.numberOfQuestions && errors.numberOfQuestions.type === "required" && (
-          <div className="error">You must enter the number of questions</div>
-        )}
-        </div>
-        <div className="difficulty">
-          <select name="difficulty" ref={register} 
-            onChange={(e) => {
-              setUserSetting({
-                ...newSetting,
-                difficulty: String(e.target.value),
-            })}}>
+                  difficulty: String(e.target.value),
+              })}}
+            >
               <option value={Difficulty.EASY}>Easy</option>
               <option value={Difficulty.MEDIUM}>Medium</option>
               <option value={Difficulty.HARD}>Hard</option>
-          </select>
-        </div>
-        <div className="category">
-          <select name="category" ref={register} 
-            onChange={(e) => {
-              setUserSetting({
-                ...newSetting,
-                category: Number(e.target.value),
-                categoryName: String(e.target.value)
-            })}}>
-                {categories.map((category) =>
-                <option key={category.id} value={category.id}>
-                  {category.name}
-                </option>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.textFields}>
+            <InputLabel htmlFor="grouped-native-select">
+              Select Category
+            </InputLabel>
+            <Select
+              id="grouped-native-select"
+              native
+              onChange={(e) => {
+                setUserSetting({
+                  ...newSetting,
+                  category: Number(e.target.value),
+                  categoryName: String(e.target.value)
+                })}}
+            >
+               {categories.map((category) =>
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
                 )}
-          </select>
-        </div>
-        <button type="submit">
-          Start the quiz!!
-        </button>
+            </Select>
+          </FormControl>
+          <Box pt={4} pb={1}>
+            <Button type="submit" variant="contained" color="primary">
+              Start the quiz!
+            </Button>
+          </Box>
       </form>
-    );
-  }
+    </div>
+  </div>
+  );
+}
 
 export default QuizSettingsForm
